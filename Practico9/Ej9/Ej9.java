162b32918949e1953c09077f72b717a9b5e92a30
Joaquin Tissera <phoenix_css@hotmail.com>
2024-11-04 16:04:53 -0300

    Repo update

--- Practico9/Ej9/Ej9.java
+++ Practico9/Ej9/Ej9.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class Ej9 {	
     public Integer funcionParametrosVariable(Object... params) {
+        ArrayList<Object> list = new ArrayList<>();
+        for (Object elem : params) {
+            list.add(elem);
+        }
+        System.out.println("cant parametros: " + list.size());
         return 1;
     }
 
@@ -7,5 +14,9 @@ public class Ej9 {
         Ej9 ob = new Ej9();
         Integer res = ob.funcionParametrosVariable(1, 2, 3);
         System.err.println(res);
+        Integer res1 = ob.funcionParametrosVariable(1, 2, 3, 4);
+        System.err.println(res1);
+        Integer res2 = ob.funcionParametrosVariable(1, 2, 3, 4,5 );
+        System.err.println(res2);
     }
 }
